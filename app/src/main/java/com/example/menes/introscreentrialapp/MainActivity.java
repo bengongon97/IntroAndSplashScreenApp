add012593816d305a11f12ef7a38d355ba74e5bb
menes <menes@sabanciuniv.edu>
2018-07-16 10:06:41 +0300

    This is when a basic notification works along with the splash screen and intro screen. App icon is done. Now to do better notification system.

--- app/src/main/java/com/example/menes/introscreentrialapp/MainActivity.java
+++ app/src/main/java/com/example/menes/introscreentrialapp/MainActivity.java
@@ -1,16 +1,73 @@
 package com.example.menes.introscreentrialapp;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String CHANNEL_ID = "1";
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Some channel name";
+            String description = "Some channel description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this,"IT IS OK, CHILL BOY!!!", Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this,"IT IS OK, CHILL BOY!!!", Toast.LENGTH_SHORT).show();
+
+        Button notifyButton = findViewById(R.id.notifyButton);
+
+
+        notifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                 int notificationId = 1;
+
+                createNotificationChannel();
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                final PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("Let's get back!")
+                        .setContentText("This will redirect you to the same page! For now...")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+                // notificationId is a unique int for each notification that you must define
+                notificationManager.notify(notificationId, mBuilder.build());
+            }
+        });
     }
 }
