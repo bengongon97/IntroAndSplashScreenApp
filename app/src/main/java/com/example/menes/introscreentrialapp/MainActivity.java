986f1027af3b90b5981840c0d0ba0228e19ac025
menes <menes@sabanciuniv.edu>
2018-07-19 16:49:07 +0300

    Alert dialog is added. Conversation is not serious but funny.

--- app/src/main/java/com/example/menes/introscreentrialapp/MainActivity.java
+++ app/src/main/java/com/example/menes/introscreentrialapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.menes.introscreentrialapp;
 
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
@@ -16,6 +18,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -44,13 +48,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-      /* Locale locale = new Locale("tr","TR");
-        Configuration config = getBaseContext().getResources().getConfiguration();
-        config.locale = locale;
-        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-*/
-
-        // Toast.makeText(this,"IT IS OK, CHILL BOY!!!", Toast.LENGTH_SHORT).show();
 
         Button notifyButton = findViewById(R.id.notifyButton);
 
@@ -69,17 +66,6 @@ public class MainActivity extends AppCompatActivity {
                 final PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
 
 
-                //set intents and pending intents to call service on click of "dismiss" action button of notification
-                /*Intent dismissIntent = new Intent(MainActivity.this, MyService.class);
-                dismissIntent.setAction(ACTION_DISMISS);
-                PendingIntent piDismiss = PendingIntent.getService(MainActivity.this, 0, dismissIntent, 0);
-
-//set intents and pending intents to call service on click of "snooze" action button of notification
-                Intent snoozeIntent = new Intent(MainActivity.this, MyService.class);
-                snoozeIntent.setAction(ACTION_SNOOZE);
-                PendingIntent piSnooze = PendingIntent.getService(MainActivity.this, 0, snoozeIntent, 0);*/
-
-
                 NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
                         .setSmallIcon(R.drawable.ovidos_logo)
                         .setContentTitle(getString(R.string.notificationTitle))
@@ -88,14 +74,9 @@ public class MainActivity extends AppCompatActivity {
                         .setTicker("New Message Alert!") //Accessibility services
                         .setPriority(NotificationCompat.PRIORITY_HIGH)
                         .setContentIntent(pendingIntent)
-                        /*.setGroupSummary(true)
-                        .setGroup(KEY_NOTIFICATION_GROUP)*/
                         .setAutoCancel(true)
                         .setColor(ContextCompat.getColor(getBaseContext(), R.color.dot_light_screen1));
-                        /*.addAction(R.drawable.ic_food,
-                                "Dismiss but Food", piDismiss)
-                        .addAction(R.drawable.ic_movie,
-                                "Snooze but Movie", piSnooze);*/
+
 
 
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
@@ -128,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
                     languageToLoad = "tr";
                     country = "TR";
                 }
-
+ 
                 Locale locale = new Locale(languageToLoad, country);
                 Locale.setDefault(locale);
                 Configuration config = new Configuration();
@@ -143,5 +124,39 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        Button popUp =  findViewById(R.id.popUpbuton);
+
+        popUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle(getString(R.string.titlePopup));
+                builder.setMessage(getString(R.string.messagePopup));
+
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, getString(R.string.goodToast),Toast.LENGTH_LONG).show();
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, getString(R.string.reallyToast),Toast.LENGTH_LONG).show();
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
     }
 }
